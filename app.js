da91dba698dfc5ffbdeda3ad5000480e4d107abe
GreyFrontendDev <greyfrontenddev@gmail.com>
2026-02-27 22:57:58 +0000

    added imports and values, const and functions

---
+++ app.js
@@ -0,0 +1,32 @@
+// How to import code
+// import { apiKey } from './util.js';
+
+// Multiple export
+// import { apiKey, abc } from './util.js';
+
+// Multiple export but as an object
+import * as util from './util.js';
+
+// Using the default:
+// import apiKey from './util.js';
+
+console.log(util.abc); // output would be the value stored in apiKey in util.js
+
+const userMessage = 'Hello World!!!';
+console.log(userMessage);
+console.log(1 + 2);
+
+function greet(userName, message = 'Hello') {
+  //   console.log('Hello!');
+  return 'Hi, I am' + userName + '.' + message;
+}
+
+function createGreeting(userName, message = 'Hello') {
+  //   console.log('Hello!');
+  return 'Hi, I am' + userName + '.' + message;
+}
+
+// greet('Max');
+// greet('Max', 'Hello max');
+const greeting1 = createGreeting('max');
+console.log(greeting1);
---
+++ util.js
@@ -0,0 +1,11 @@
+// How to export code
+
+// Different ways to export your code 1
+// export let apiKey = 'adnasdonadlkad';
+
+// Use the default
+// export default 'adnasdonadlkad';
+
+// Multiple named exports
+export let apiKey = 'adnasdonadlkad';
+export let abc = 'abicd';
